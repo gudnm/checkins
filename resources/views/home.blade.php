db740ea0bdf4f5cb347ee062bdcc5a019f20221d
gudnm <dfobox@gmail.com>
2017-05-08 14:02:21 -0400

    Change home route to / and update app name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,22 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Check in today</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <a href="checkin">Take a questionnaire</a>
+                </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Your history</div>
+
+                <div class="panel-body">
+                    ...is here
                 </div>
             </div>
         </div>
